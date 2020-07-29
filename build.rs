a0bdd760bbb2a1ea2ae7ee9536db8b63716e47a6
Mike English <mike.english@gmail.com>
2020-07-29 02:38:01 -0400

    Suppress unused warnings in imported C code

--- build.rs
+++ build.rs
@@ -4,5 +4,6 @@ fn main() {
     cc::Build::new().file("src/c/src/util/tunnel.c")
         .include("src/c/third_party/assert-macros")
         .include("src/c/third_party/pt")
+        .extra_warnings(false)
         .compile("tunnel");
 }
